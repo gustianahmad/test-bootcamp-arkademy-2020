2c54f97220835df346a51c30277e234b0fad98f5
gustianahmad <gustian.ahmadfauzan@gmail.com>
2020-01-18 18:15:48 +0700

    test

---
+++ test-bootcamp-arkademy-2020/soal_no_2/2.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE gustian;
+
+CREATE TABLE IF NOT EXISTS `users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(50) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `password` varchar(50) NOT NULL,
+ PRIMARY KEY (`id`)
+ );
